b8f614902656723874906121d50155b9ef8a15ac
Caitria O'Neill <caitria.e.oneill@gmail.com>
2014-03-04 23:26:17 -0800

    Made Image View responsive, some other image view styling

--- scripts/ea.js
+++ scripts/ea.js
@@ -227,7 +227,7 @@ $(function() {
       return (<div className="detail">
                 <a href="#/"><div className="overlay"></div></a>
                 <div className="image-detail">
-                  <img src={this.props.data.image.url} />
+                  <img src={this.props.data.image.url} className="image-large"/>
                   <div className="detail-panel">
                     <div className="detail-header">
                       <img src={this.props.data.user.profile_picture} />
@@ -289,7 +289,7 @@ $(function() {
     render: function() {
       return (<div className="comments">
                 <div>
-                  <ul>
+                  <ul className="comment-source">
                     {_.map(_.keys(this.props.comments).sort(), function(type) {
                     var classes = React.addons.classSet(
                       {active: type === this.props.active});
